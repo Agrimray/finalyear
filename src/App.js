3c611454724b87608fdb6c88711b1119cc8b6f00
Agrimray <69646734+Agrimray@users.noreply.github.com>
2023-02-26 01:53:55 +0530

    jj
    hh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Teamcreation from './components/Teamcreation';
+import Sidebar from './components/Sidebar';
+import Viewplayer from './components/Viewplayer';
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   
+    
+  <Router>
+    
+    <Navbar title="EDGED" class="center"/>
+    <div className="container my-3">
+    <Routes>
+           
+            <Route exact path="/login" element={<Login />}>{/* Switches are replaced by Routes*/}
+            </Route>
+            <Route exact path="/signup" element={<Signup/>}>{/* Switches are replaced by Routes*/}
+            </Route>
+            
+    </Routes>
+    <br>
+    </br>
+    <Sidebar/>
+    <Routes>
+            <Route exact path="/teamcreation" element={<Teamcreation />}>{/* Switches are replaced by Routes*/}
+            </Route>
+            <Route exact path="/viewplayer" element={<Viewplayer />}>{/* Switches are replaced by Routes*/}
+            </Route>
+    </Routes>
+    
     </div>
+  </Router> 
+   
+   
+   
+   </>
   );
 }
 
